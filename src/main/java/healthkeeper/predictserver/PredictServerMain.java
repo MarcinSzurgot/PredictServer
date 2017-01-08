167cf3a8c0df0137a6c6e3bd8b25093e083e73f2
Marcin Szurgot <ramcin908@wp.pl>
2017-01-08 16:57:05 +0100

    Initial commit

---
+++ src/main/java/healthkeeper/predictserver/PredictServerMain.java
@@ -0,0 +1,10 @@
+package main.java.healthkeeper.predictserver;
+
+public class PredictServerMain {
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    
+    }
+
+}
